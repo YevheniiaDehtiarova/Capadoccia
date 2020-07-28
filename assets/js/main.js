4419c4bafe8ea5fa62f304972dcb4e05492d34e0
Evgeniya <jeni4ka2509@gmail.com>
2020-07-28 20:31:19 +0300

    complete media partly

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,15 @@ $(function() {
         prevArrow: '<button type="button" class=" slick-btn slick-prev"><img src="assets/images/arrow-prev.svg" alt=""></button>',
         nextArrow: '<button type="button" class=" slick-btn slick-next"><img src="assets/images/arrow-next.svg" alt=""></button>',
         autoplay: true,
-        fade: true
+        fade: true,
+        //адаптивность для слайре из сайта слик слайдер
+        responsive: [{
+            breakpoint: 601,
+            settings: {
+                arrows: false //отключаем стрелки
+
+            }
+        }, ]
     });
 
 });
